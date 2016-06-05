87299381b95e8a099c6d99177465ec64a1fe0df7
Chaddy <kumbirair@yahoo.com>
2016-06-05 22:24:03 +0200

    updated credit value

--- notification.php
+++ notification.php
@@ -21,7 +21,7 @@ $current_user = Backendless::$UserService->login($_POST["username"], $_POST["pas
 //var_dump($user);
 //echo $current_user->getProperty("mari");
 
-$current = intval($current_user->getProperty("mari"));
+$current = (intval($current_user->getProperty("mari")))*100;
 $new_bal = $current+intval($_POST["package"]);
 
 
