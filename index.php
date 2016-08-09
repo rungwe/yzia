ce0ff021919dacc4e49edbe665ed2f290f384c48
Chaddy <kumbirair@yahoo.com>
2016-08-09 04:18:56 +0200

    new interface

--- index.php
+++ index.php
@@ -1,67 +1,320 @@
-<html>
+ <!-- FlatFy Theme - Andrea Galanti /-->
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="Flatfy Free Flat and Responsive HTML5 Template ">
+    <meta name="author" content="">
 
-	<head>
-		<title>Yzia</title>
-		<link rel="icon" href="img/logo.png" />
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="css/yzia.css" />
-		<script src="js/jquery-2.1.1.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script src="js/validate.js"></script>
-		
-		
-	</head>
+    <title>Yzia - Welcome</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+    <!-- Custom Google Web Font -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
 	
-	<body style="background-image:url(img/bg2.jpg);background-repeat:no-repeat;background-attachment:fixed;height:100%;background-size:cover;">
+    <!-- Custom CSS-->
+    <link href="css/general.css" rel="stylesheet">
 	
-		
-		<div class="row">
-			<div class="pull-right col-sm-1">
-				<button align="right" type="button" class="btn" data-toggle="modal" data-target="#login" style="margin-right:20px;border-radius:0px;background-color:#ff4081;color:white;margin-top:10px;">Login</button>
+	 <!-- Owl-Carousel -->
+    <link href="css/custom.css" rel="stylesheet">
+	<link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/animate.css" rel="stylesheet">
+	
+	<!-- Magnific Popup core CSS file -->
+	<link rel="stylesheet" href="css/magnific-popup.css"> 
+	
+	<script src="js/modernizr-2.8.3.min.js"></script>  <!-- Modernizr /-->
+	<!--[if IE 9]>
+		<script src="js/PIE_IE9.js"></script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+		<script src="js/PIE_IE678.js"></script>
+	<![endif]-->
+
+	<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+	<![endif]-->
+
+</head>
+
+<body id="home">
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div id="status"></div>
+	</div>
+	
+	<!-- FullScreen -->
+    <div class="intro-header">
+		<div class="col-xs-12 text-center abcen1">
+			<h1 class="h1_home wow fadeIn" data-wow-delay="0.4s">Yzia</h1>
+			<h3 class="h3_home wow fadeIn" data-wow-delay="0.6s" style="color:#ff4081;font-weight:600;">Smooth music streaming services</h3>
+			<ul class="list-inline intro-social-buttons">
+				<li><a href="" class="btn  btn-lg  wow fadeIn" data-wow-delay="0.8s"><span class="network-name"><img src="img/googleplay.png" height="80px"/></span></a>
+				</li>
+				<li id="download" ><a href="" class="btn  btn-lg  wow fadeIn" data-wow-delay="0.8s"><span class="network-name"><img src="img/apple.png" height="70px"/></span></a>
+				</li>
+			</ul>
+		</div>    
+        <!-- /.container -->
+		<div class="col-xs-12 text-center abcen wow fadeIn">
+			<div class="button_down "> 
+				<a class="imgcircle wow bounceInUp" data-wow-duration="1.5s"  href="#whatis"> <img class="img_scroll" src="img/icon/circle.png" alt=""> </a>
 			</div>
-			<div class="col-sm-offset-1">
-				<img src="img/logo.png"/ height="80px">
-			</div>
-			</div>
-		
 		</div>
-		<div class="row" >
-		
-			<div class="col-md-offset-1 col-md-5 " style="margin-top:8%;">
-				<h1 style="color:white;font-weight:700;line-weight:1.2;">Yzia music streaming</h1>
-				<br>
-				<h4 style="font-weight:600;color:gray;"><b>Buy Credits for your music streaming here and let the fun begin.<br>Register as music artist and get started with us</b></h4>
-				<br>
-				<div class="cd-triggers">
-					<button class="btn btn-lg" style="border-radius:0px;background-color:#ff4081;color:white;" data-toggle="modal" data-target="#purchase">Buy credit$</button>
-					<button class="btn btn-lg" style="border-radius:0px;background-color:#ff4081;color:white;" data-toggle="modal" data-target="#register">Register</button>
-				</div>
-			</div> <!-- cd-product-intro -->
-		
-			<div class="col-md-7 pull-right" style="margin-top:-20%;margin-right:10%;">
-				<div class="cd-image-container pull-right" >
-					<div>
-						<div class="cd-phone-frame"></div>
-						<div class="cd-image-wrapper">
-							<img src="img/slide1.gif" data-video="video/video-1" alt="Screen Preview 1">
-						</div>
-					</div>
-				</div>
+    </div>
+	
+	<!-- NavBar-->
+	<nav class="navbar-default" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#home">Yzia</a>
+			</div>
+
+			<div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					
+					<li class="menuItem"><a href="#whatis">What is Yzia?</a></li>
+					<li class="menuItem"><a href="#screen">Screenshot</a></li>
+					<li class="menuItem"><a href="#contact">Contact</a></li>
+					<li class="menuItem"><a  data-toggle="modal" data-target="#purchase">Buy credits</a></li>
+					<li class="menuItem"><a data-toggle="modal" data-target="#login">Login</a></li>
+				</ul>
+			</div>
+		   
+		</div>
+	</nav> 
+	
+	<!-- What is -->
+	<div id="whatis" class="content-section-b" style="border-top: 0">
+		<div class="container">
+
+			<div class="col-md-6 col-md-offset-3 text-center wrap_title">
+				<h2>What is?</h2>
+				<p class="lead" style="margin-top:0">A mobile music streaming application, that is found on both android and ios platform, designed with a goal to promote local artists</p>
+				
 			</div>
 			
-		</div>
-		
-		<div class="row app-store" style="">
-				<div class="col-sm-offset-3 col-sm-4">
-					<img src="img/googleplay.png" height="80" width="210"/>
-				</div>
-				<div class="col-sm-3">
-					<img src="img/apple.png" height="70" width="210" style="margin-left:-50%;"/>
-				</div>
-		</div>
-		
+			<div class="row">
+			
+				<div class="col-sm-4 wow fadeInDown text-center">
+				  <img class="rotate" src="img/icon/heart.svg" alt="Generic placeholder image">
+				  <h3>Follow artists</h3>
+				  <p class="lead">Follow your favourite local artists and get updated with their music and events </p>
 
-		<!-- Modal Buy-->
+				  <!-- <p><a class="btn btn-embossed btn-primary view" role="button">View Details</a></p> -->
+				</div><!-- /.col-lg-4 -->
+				
+				<div class="col-sm-4 wow fadeInDown text-center">
+				  <img  class="rotate" src="img/icon/headphones.svg" alt="Generic placeholder image">
+				   <h3>Buy music</h3>
+				   <p class="lead">Purchase songs in the app and continue to listen even when you are offline, lets promote our art industry </p>
+				   <!-- <p><a class="btn btn-embossed btn-primary view" role="button">View Details</a></p> -->
+				</div><!-- /.col-lg-4 -->
+				
+				<div class="col-sm-4 wow fadeInDown text-center">
+				  <img  class="rotate" src="img/icon/beacon.svg" alt="Generic placeholder image">
+				   <h3>Local radio</h3>
+					<p class="lead">Access local radio stations wherever you are, you got your radio on the fly, any time </p>
+				  <!-- <p><a class="btn btn-embossed btn-primary view" role="button">View Details</a></p> -->
+				</div><!-- /.col-lg-4 -->
+				
+			</div><!-- /.row -->
+				
+			<div class="row tworow">
+			
+				<div class="col-sm-4  wow fadeInDown text-center">
+				  <img class="rotate" src="img/icon/mic.svg" alt="Generic placeholder image">
+				  <h3>Publish music</h3>
+				  <p class="lead">Are you a local artist? We got you homie, publish your music and get paid easily, we handle all the admin overhead </p>
+				 <!-- <p><a class="btn btn-embossed btn-primary view" role="button">View Details</a></p> -->
+				</div><!-- /.col-lg-4 -->
+				
+				<div class="col-sm-4 wow fadeInDown text-center">
+				  <img  class="rotate" src="img/icon/calendar.svg" alt="Generic placeholder image">
+				   <h3>Events</h3>
+				   <p class="lead">Get access to events such as music shows, concert, parties that are happening, don't get left behind </p>
+				   <!-- <p><a class="btn btn-embossed btn-primary view" role="button">View Details</a></p> -->
+				</div><!-- /.col-lg-4 -->
+				
+				<div class="col-sm-4 wow fadeInDown text-center">
+				  <img  class="rotate" src="img/icon/speed.svg" alt="Generic placeholder image">
+				   <h3>Quality streaming</h3>
+				 <p class="lead">Very fast audio streaming, supported by morden and latest technology in the industry, and our content is highly distributed globally guaranteeing speed at all locations </p>
+				  <!-- <p><a class="btn btn-embossed btn-primary view" role="button">View Details</a></p> -->
+				</div><!-- /.col-lg-4 -->
+				
+			</div><!-- /.row -->
+		</div>
+	</div>
+	
+
+	<!-- Screenshot -->
+	<div id="screen" class="content-section-b">
+        <div class="container">
+          <div class="row" >
+			 <div class="col-md-6 col-md-offset-3 text-center wrap_title ">
+				<h2>Screen App</h2>
+				
+			 </div>
+		  </div>
+		    <div class="row wow bounceInUp" >
+              <div id="owl-demo" class="owl-carousel">
+				
+				<a href="img/screen1.jpg" class="image-link">
+					<div class="item">
+						<img  class="img-responsive img-rounded" src="img/screen1.jpg" alt="Owl Image">
+					</div>
+				</a>
+				
+               <a href="img/screen2.jpg" class="image-link">
+					<div class="item">
+						<img  class="img-responsive img-rounded" src="img/screen2.jpg" alt="Owl Image">
+					</div>
+				</a>
+				
+				<a href="img/screen3.jpg" class="image-link">
+					<div class="item">
+						<img  class="img-responsive img-rounded" src="img/screen3.jpg" alt="Owl Image">
+					</div>
+				</a>
+				
+				<a href="img/screen4.jpg" class="image-link">
+					<div class="item">
+						<img  class="img-responsive img-rounded" src="img/screen4.jpg" alt="Owl Image">
+					</div>
+				</a>
+				
+               <a href="img/screen5.jpg" class="image-link">
+					<div class="item">
+						<img  class="img-responsive img-rounded" src="img/screen5.jpg" alt="Owl Image">
+					</div>
+				</a>
+				
+				<a href="img/screen6.jpg" class="image-link">
+					<div class="item">
+						<img  class="img-responsive img-rounded" src="img/screen6.jpg" alt="Owl Image">
+					</div>
+				</a>
+              </div>       
+          </div>
+        </div>
+
+
+	</div>
+	
+	<div  class="content-section-c ">
+		<div class="container">
+			<div class="row">
+			<div class="col-md-6 col-md-offset-3 text-center white">
+				<h2>Get Live Updates</h2>
+				
+			 </div>
+			<div class="col-md-6 col-md-offset-3 text-center">
+				<div class="mockup-content">
+						<div class="morph-button wow pulse morph-button-inflow morph-button-inflow-1">
+							<button type="button "><span>Subscribe to our Newsletter</span></button>
+							<div class="morph-content">
+								<div>
+									<div class="content-style-form content-style-form-4 ">
+										<h2 class="morph-clone">Subscribe to our Newsletter</h2>
+										<form>
+											<p><label>Your Email Address</label><input type="text"/></p>
+											<p><button>Subscribe me</button></p>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+				</div>
+			</div>	
+			</div>
+		</div>
+	</div>	
+	
+	
+	
+	<!-- Banner Download -->
+	<div id="downloadlink" class="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center wrap_title">
+				<h2>Download Free</h2>
+				<ul class="list-inline intro-social-buttons">
+				<li><a href="" class="btn  btn-lg  wow fadeIn" data-wow-delay="0.8s"><span class="network-name"><img src="img/googleplay.png" height="80px"/></span></a>
+				</li>
+				<li id="download" ><a href="" class="btn  btn-lg  wow fadeIn" data-wow-delay="0.8s"><span class="network-name"><img src="img/apple.png" height="70px"/></span></a>
+				</li>
+			</ul>
+			 </div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- Contact -->
+	<div id="contact" class="content-section-a">
+		<div class="container">
+			<div class="row">
+			
+			<div class="col-md-6 col-md-offset-3 text-center wrap_title">
+				<h2>Contact Us</h2>
+				<p class="lead" style="margin-top:0">Get in touch with us.</p>
+			</div>
+			
+			<form role="form" action="" method="post" >
+				<div class="col-md-6">
+					<div class="form-group">
+						<label for="InputName">Your Name</label>
+						<div class="input-group">
+							<input type="text" class="form-control" name="InputName" id="InputName" placeholder="Enter Name" required>
+							<span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label for="InputEmail">Your Email</label>
+						<div class="input-group">
+							<input type="email" class="form-control" id="InputEmail" name="InputEmail" placeholder="Enter Email" required  >
+							<span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+						</div>
+					</div>
+					
+					<div class="form-group">
+						<label for="InputMessage">Message</label>
+						<div class="input-group">
+							<textarea name="InputMessage" id="InputMessage" class="form-control" rows="5" required></textarea>
+							<span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
+						</div>
+					</div>
+
+					<input type="submit" name="submit" id="submit" value="Submit" class="btn wow tada btn-embossed btn-primary pull-right">
+				</div>
+			</form>
+			
+			
+				
+			</div>
+		</div>
+	</div>
+	
+	<!-- Modal Buy-->
 		<div class="modal fade" id="purchase" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 		  <div class="modal-dialog" role="document">
 			<div class="modal-content">
@@ -203,9 +456,33 @@
 			</div>
 		  </div>
 		</div>
-		
-	</body>
 
-
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+	<script src="js/owl.carousel.js"></script>
+	<script src="js/script.js"></script>
+	<!-- StikyMenu -->
+	<script src="js/stickUp.min.js"></script>
+	<script type="text/javascript">
+	  jQuery(function($) {
+		$(document).ready( function() {
+		  $('.navbar-default').stickUp();
+		  
+		});
+	  });
+	
+	</script>
+	<!-- Smoothscroll -->
+	<script type="text/javascript" src="js/jquery.corner.js"></script> 
+	<script src="js/wow.min.js"></script>
+	<script>
+	 new WOW().init();
+	</script>
+	<script src="js/classie.js"></script>
+	<script src="js/uiMorphingButton_inflow.js"></script>
+	<!-- Magnific Popup core JS file -->
+	<script src="js/jquery.magnific-popup.js"></script> 
+</body>
 
 </html>
---
+++ index_old.php
@@ -0,0 +1,211 @@
+<html>
+
+	<head>
+		<title>Yzia</title>
+		<link rel="icon" href="img/logo.png" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/yzia.css" />
+		<script src="js/jquery-2.1.1.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="js/validate.js"></script>
+		
+		
+	</head>
+	
+	<body style="background-image:url(img/bg2.jpg);background-repeat:no-repeat;background-attachment:fixed;height:100%;background-size:cover;">
+	
+		
+		<div class="row">
+			<div class="pull-right col-sm-1">
+				<button align="right" type="button" class="btn" data-toggle="modal" data-target="#login" style="margin-right:20px;border-radius:0px;background-color:#ff4081;color:white;margin-top:10px;">Login</button>
+			</div>
+			<div class="col-sm-offset-1">
+				<img src="img/logo.png"/ height="80px">
+			</div>
+			</div>
+		
+		</div>
+		<div class="row" >
+		
+			<div class="col-md-offset-1 col-md-5 " style="margin-top:8%;">
+				<h1 style="color:white;font-weight:700;line-weight:1.2;">Yzia music streaming</h1>
+				<br>
+				<h4 style="font-weight:600;color:gray;"><b>Buy Credits for your music streaming here and let the fun begin.<br>Register as music artist and get started with us</b></h4>
+				<br>
+				<div class="cd-triggers">
+					<button class="btn btn-lg" style="border-radius:0px;background-color:#ff4081;color:white;" data-toggle="modal" data-target="#purchase">Buy credit$</button>
+					<button class="btn btn-lg" style="border-radius:0px;background-color:#ff4081;color:white;" data-toggle="modal" data-target="#register">Register</button>
+				</div>
+			</div> <!-- cd-product-intro -->
+		
+			<div class="col-md-7 pull-right" style="margin-top:-20%;margin-right:10%;">
+				<div class="cd-image-container pull-right" >
+					<div>
+						<div class="cd-phone-frame"></div>
+						<div class="cd-image-wrapper">
+							<img src="img/slide1.gif" data-video="video/video-1" alt="Screen Preview 1">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		
+		<div class="row app-store" style="">
+				<div class="col-sm-offset-3 col-sm-4">
+					<img src="img/googleplay.png" height="80" width="210"/>
+				</div>
+				<div class="col-sm-3">
+					<img src="img/apple.png" height="70" width="210" style="margin-left:-50%;"/>
+				</div>
+		</div>
+		
+
+		<!-- Modal Buy-->
+		<div class="modal fade" id="purchase" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Buy Credits</h4>
+			  </div>
+			  <form role="form" action="payment.php" method="POST">
+			  <div class="modal-body">
+				
+				  <div class="form-group">
+					<label for="email">Email address:</label>
+					<input type="email" name="email" class="form-control" id="payment-email" required>
+				  </div>
+				  <div class="form-group">
+					<label for="username">Username:</label>
+					<input type="text" name="username" class="form-control" id="username" required>
+				  </div>
+				   <div class="form-group">
+					<label for="phone">Phone:</label>
+					<input type="text" name="phone" class="form-control" id="phone" required>
+				  </div>
+				  
+				  <div class="form-group">
+					<label for="package">Package:</label>
+					<select name="package" class="form-control">
+					  <option value="5">Economic ($5)</option>
+					  <option value="10">Standard ($10)</option>
+					  <option value="15">Premium ($15)</option>
+					</select>
+				  </div>
+				  <div class="row">
+					<div class="col-sm-2">
+						<img src="img/eco.png" height="50"/>
+					</div>
+					<div class="col-sm-2">
+						<img src="img/tele.jpg" height="50"/>
+					</div>
+					<div class="col-sm-2">
+						<img src="img/master.png" height="50"/>
+					</div>
+					<div class="col-sm-2">
+						<img src="img/visa.png" height="50"/>
+					</div>
+					<div class="col-sm-2">
+						<img src="img/one.jpg" height="50"/>
+					</div>
+					
+				  </div>
+				
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				<button type="submit" class="btn">Purchase</button>
+			  </div>
+			  </form>
+			</div>
+		  </div>
+		</div>
+		
+		
+		<!-- Modal login-->
+		<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Login as an artist</h4>
+			  </div>
+			  <div class="modal-body">
+				<form role="form" id="login" method="POST" action="aunthenticate.php">
+				  <div class="form-group">
+					<label for="email">Username:</label>
+					<input type="text" name="username" class="form-control" id="email">
+				  </div>
+				  <div class="form-group">
+					<label for="pwd">Password:</label>
+					<input type="password" value="password" class="form-control" style="outline-color: #ff4081 !mportant;" id="pwd">
+				  </div>
+				  <div class="checkbox">
+					<label><input type="checkbox"> Remember me</label>
+				  </div>
+				  
+				</form>
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				<button type="button" class="btn" onclick="login()">Login</button>
+			  </div>
+			</div>
+		  </div>
+		</div>
+		
+		<!-- Modal Register-->
+		<div class="modal fade" id="register" tabindex="-1" role="dialog" aria-labelledby="regModal">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Artist Registration</h4>
+			  </div>
+			  <div class="modal-body">
+				<form role="form" id="register">
+				  <div class="form-group">
+					<label for="email">Email address:</label>
+					<input type="email" class="form-control" id="reg-email">
+				  </div>
+				  <div class="form-group">
+					<label for="phone">First Name:</label>
+					<input type="text" class="form-control" id="phone">
+				  </div>
+				  <div class="form-group">
+					<label for="phone">Last Name:</label>
+					<input type="text" class="form-control" id="phone">
+				  </div>
+				   <div class="form-group">
+					<label for="phone">Phone:</label>
+					<input type="text" class="form-control" id="phone">
+				  </div>
+				  <div class="form-group">
+					<label for="phone">Stage Name:</label>
+					<input type="text" class="form-control" id="phone">
+				  </div>
+				  <div class="form-group">
+					<label for="pwd">Password:</label>
+					<input type="password" class="form-control" style="outline-color: #ff4081 !mportant;" id="reg-pwd">
+				  </div>
+				  <div class="form-group">
+					<label for="pwd">Confirm Password:</label>
+					<input type="password" class="form-control" style="outline-color: #ff4081 !mportant;" id="reg-pwd-con">
+				  </div>
+				  
+				</form>
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				<button type="button" class="btn">Register</button>
+			  </div>
+			</div>
+		  </div>
+		</div>
+		
+	</body>
+
+
+
+</html>
